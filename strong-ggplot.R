1913e58b66e336c693751b8ba1704729808ae670
Jacob <jacobwdink@gmail.com>
2016-05-09 11:31:30 -0500

    updated strong theme

--- strong-ggplot.R
+++ strong-ggplot.R
@@ -5,7 +5,6 @@ theme_strong <- theme_bw() +
                       axis.text = element_text(size = 14),
                       axis.title = element_text(size = 16, family="SFNS Display"),
                       text = element_text(family="SFNS Display"),
-                      legend.key = element_rect(fill = "navy"),
                       legend.background = element_rect(fill = "white"),
                       panel.grid.major = element_blank(),
                       panel.grid.minor = element_blank(),
@@ -13,5 +12,8 @@ theme_strong <- theme_bw() +
                       strip.background = element_rect(fill = "white", colour = NA),
                       strip.text.x = element_text(size = 14, face = "bold", colour = "black")
                 )
+
+color_scale_discrete_strong <- c("#00CBFE","#2B4141","#8AB9B5","#C8C2AE")
+# use `scale_color_manual(values=color_scale_discrete_strong)`
 
 
